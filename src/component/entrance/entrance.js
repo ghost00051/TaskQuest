4f7baefcde469c429ad9c707275d6ffb0bb4f8ee
Иван <in419264@gmail.com>
2025-02-23 19:34:17 +0300

    Регистрация
    Доделано сохранение токена авторизации в LocalStorage

--- src/component/entrance/entrance.js
+++ src/component/entrance/entrance.js
@@ -1,24 +1,30 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import first from '../../img/first.svg';
 import сloseEyes from '../../img/CloseEyes.svg'
 import openEyes from '../../img/OpenEyes.svg'
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 
 const Entr = () => {
+  const navigate = useNavigate();
+
+  const handleLoginClick = () => {
+    navigate('/');
+  };
   useEffect(() => {
     const inputPS = document.getElementById('password-input');
     const eyesOpen = document.getElementById('OpenEyes');
     const eyesClose = document.getElementById('CloseEyes');
 
     if (inputPS && eyesOpen && eyesClose) {
-      eyesOpen.addEventListener('click', function() {
+      eyesOpen.addEventListener('click', function () {
         inputPS.type = 'password';
         eyesOpen.style.display = 'none';
         eyesClose.style.display = 'inline';
       });
 
-      eyesClose.addEventListener('click', function() {
+      eyesClose.addEventListener('click', function () {
         inputPS.type = 'text';
         eyesClose.style.display = 'none';
         eyesOpen.style.display = 'inline';
@@ -27,13 +33,43 @@ const Entr = () => {
 
     return () => {
       if (eyesOpen) {
-        eyesOpen.removeEventListener('click', () => {});
+        eyesOpen.removeEventListener('click', () => { });
       }
       if (eyesClose) {
-        eyesClose.removeEventListener('click', () => {});
+        eyesClose.removeEventListener('click', () => { });
       }
     };
   }, []);
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await fetch('http://localhost:5000/delivery_api/users/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: new URLSearchParams({
+          email,
+          password
+        })
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log('Login successful:', data);
+        localStorage.setItem('token', data.token);
+      } else {
+        throw new Error('Login failed');
+      }
+      
+    } catch (error) {
+      console.error('Login error:', error);
+    }
+  };
 
   return (
     <div className="header">
@@ -42,7 +78,8 @@ const Entr = () => {
         <div className="Flex-comp">
           <div className="left-block">
             <p className="Enter">Войти</p>
-            <form className='entrance-form'>
+            <form className='entrance-form'
+              onSubmit={handleSubmit}>
               <div className="input-checked input-email">
                 <label htmlFor="email-input" className="top-email">
                   Email
@@ -52,6 +89,9 @@ const Entr = () => {
                   type="email" // Исправленный тип
                   placeholder="Введите Email-адрес ...."
                   aria-label="Введите email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
                 />
               </div>
               <div className="input-checked input-password">
@@ -64,6 +104,9 @@ const Entr = () => {
                     type="password" // Исправленный тип
                     placeholder="Введите пароль"
                     aria-label="Введите пароль"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
                   />
                 </div>
                 <div className="chekPassword">
@@ -88,6 +131,7 @@ const Entr = () => {
                 className="entrance-button"
                 aria-label="Войти в аккаунт"
                 id='entrance-button'
+                onClick={handleLoginClick}
               >Войти</button>
             </form>
             <div className='registration'>
